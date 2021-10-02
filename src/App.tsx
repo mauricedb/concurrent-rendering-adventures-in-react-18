fd61d97074d6a9d8dd56deb0a16ed9694d1d38e2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-10-02 16:55:29 +0200

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import atomium from './atomium.jpg';
 
 import { NavBar } from './components/NavBar';
 import { PrimeNumbers } from './components/primes/PrimeNumbers';
@@ -17,7 +19,14 @@ function App() {
           <Route path="/primes">
             <PrimeNumbers />
           </Route>
-          <Route path="/">Home</Route>
+          <Route path="/">
+            <div className="row">
+              <h2 className="text-center mt-5">
+                Concurrent Rendering Adventures in React 18
+              </h2>
+              <img className="g-5" src={atomium} alt="React Brussels" />
+            </div>
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
