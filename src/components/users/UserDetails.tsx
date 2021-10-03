c94353fbf42afe7f949c325e92a27ddab57d1706
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-10-03 20:07:33 +0200

    Parallel <Suspense />

--- src/components/users/UserDetails.tsx
+++ src/components/users/UserDetails.tsx
@@ -14,7 +14,9 @@ export function UserDetails({ userId, movieId }: Props) {
       <h4 className="text-center mt-5">User details</h4>
       <Suspense fallback={<Loading />}>
         <AccountDetails userId={userId} />
-        <h4 className="text-center mt-5">Favorite movie</h4>
+      </Suspense>
+      <h4 className="text-center mt-5">Favorite movie</h4>
+      <Suspense fallback={<Loading />}>
         <MovieDetails movieId={movieId} />
       </Suspense>
     </div>
