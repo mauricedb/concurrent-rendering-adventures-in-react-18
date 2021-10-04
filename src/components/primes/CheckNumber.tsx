ec97a2793ba3771aa3a67c5987a44d6036da7bc4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-10-04 09:50:42 +0200

    Optional isPending

--- src/components/primes/CheckNumber.tsx
+++ src/components/primes/CheckNumber.tsx
@@ -3,17 +3,18 @@ import classes from './CheckNumber.module.css';
 
 interface Props {
   value: number;
+  isPending?: boolean;
 }
 
-// '⏳'
-
-export function CheckNumber({ value }: Props) {
+export function CheckNumber({ value, isPending }: Props) {
   return (
     <div className="col">
       <div className={`card ${classes.CheckNumber}`}>
         <div className="card-body text-center">
           <div className="fs-5">{value.toLocaleString()}</div>
-          <div className="fs-3">{isPrime(value) ? '✔️' : '❌'}</div>
+          <div className="fs-3">
+            {isPending ? '⏳' : isPrime(value) ? '✔️' : '❌'}
+          </div>
         </div>
       </div>
     </div>
