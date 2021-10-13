15e2ba73dfa6eda859c619682a3225ab12cb5e38
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-10-13 09:36:43 +0200

    <Suspense /> & Transitions

--- src/components/users/UserDetails.tsx
+++ src/components/users/UserDetails.tsx
@@ -1,26 +1,25 @@
-import { Suspense, SuspenseList } from 'react';
-import { Loading } from '../Loading';
 import { AccountDetails } from './AccountDetails';
 import { MovieDetails } from './MovieDetails';
 
 interface Props {
   userId: number;
   movieId: number;
+  isPending?: boolean;
 }
 
-export function UserDetails({ userId, movieId }: Props) {
+export function UserDetails({ userId, movieId, isPending }: Props) {
   return (
-    <div>
-      <SuspenseList revealOrder="together">
-        <h4 className="text-center mt-5">User details</h4>
-        <Suspense fallback={<Loading />}>
-          <AccountDetails userId={userId} />
-        </Suspense>
-        <h4 className="text-center mt-5">Favorite movie</h4>
-        <Suspense fallback={<Loading />}>
-          <MovieDetails movieId={movieId} />
-        </Suspense>
-      </SuspenseList>
+    <div style={{ opacity: isPending ? 0.25 : undefined }}>
+      {/* <SuspenseList revealOrder="together"> */}
+      <h4 className="text-center mt-5">User details</h4>
+      {/* <Suspense fallback={<Loading />}> */}
+      <AccountDetails userId={userId} />
+      {/* </Suspense> */}
+      <h4 className="text-center mt-5">Favorite movie</h4>
+      {/* <Suspense fallback={<Loading />}> */}
+      <MovieDetails movieId={movieId} />
+      {/* </Suspense> */}
+      {/* </SuspenseList> */}
     </div>
   );
 }
--- src/components/users/UserList.tsx
+++ src/components/users/UserList.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import useSWR from 'swr';
 import type { Account } from '../../types';
 import { UserDetails } from './UserDetails';
 
 export function UserList() {
+  const [isPending, startTransition] = useTransition();
   const [selectedUser, setSelectedUser] = useState<Account | null>(null);
+  const [selectedUserId, setSelectedUserId] = useState(NaN);
   const { data } = useSWR<Account[], Error>(
     `${process.env.REACT_APP_API_BASE}/accounts`
   );
@@ -24,8 +26,12 @@ export function UserList() {
             >
               <button
                 className="btn shadow-none"
-                onClick={() => setSelectedUser(user)}
+                onClick={() => {
+                  setSelectedUserId(user.id);
+                  startTransition(() => setSelectedUser(user));
+                }}
               >
+                {isPending && selectedUserId === user.id && '⏳ '}
                 {user.firstname}
                 &nbsp;
                 {user.surname}
@@ -40,6 +46,7 @@ export function UserList() {
             key={selectedUser.id}
             userId={selectedUser.id}
             movieId={selectedUser.favorite_movie}
+            isPending={isPending}
           />
         )}
       </div>
